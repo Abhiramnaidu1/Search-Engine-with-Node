d0a8490d5eece90b126db54411dfb3c81d910977
Abhiramnaidu1 <nkank001@odu.edu>
2021-03-02 03:25:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 require('./config/config');
 require('./models/db');
+require('./config/passportConfig');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const passport = require('passport');
 const rtsIndex = require('./routes/index.router');
 
-var passport = require("passport-local"),
-    passportLocalMongoose =
-        require("passport-local-mongoose"),
-    User = require("./models/user.model");
-
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost:27017/project");
+User = require("./models/user.model");
 var app = express();
 
 // middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use(passport.initialize());
 app.use('/api', rtsIndex);
 
 app.use(express.static('views'));
@@ -31,23 +24,7 @@ app.get("/login", function (req, res) {
     res.render("login");
 });
 
-//Handling user login
-app.post("/login", passport.authenticate("local", {
-    successRedirect: "/secret",
-    failureRedirect: "/login"
-}), function (req, res) {
-});
 
-//Handling user logout
-app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/");
-});
-
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect("/login");
-}
 // error handler
 app.use((err, req, res, next) => {
     if (err.name === 'ValidationError') {

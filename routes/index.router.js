24fe6e43f748033e4009644a56a0ace2631aa63c
Abhiramnaidu1 <nkank001@odu.edu>
2021-03-04 13:58:09 -0500

    Update index.router.js

--- routes/index.router.js
+++ routes/index.router.js
@@ -7,5 +7,8 @@ const jwtHelper = require('../config/jwtHelper');
 router.post('/register', ctrlUser.register);
 router.post('/authenticate', ctrlUser.authenticate);
 router.get('/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile);
+router.post('/forgot_password',ctrlUser.forgot_password);
+router.post('/reset',ctrlUser.reset_password);
+
 //router.post('/login',ctrlUser.login);
 module.exports = router;

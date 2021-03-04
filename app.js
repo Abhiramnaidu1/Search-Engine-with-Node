c394bdc5847f8e632c56698ba7d76fb587c1606f
Abhiramnaidu1 <nkank001@odu.edu>
2021-03-04 14:35:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(passport.initialize());
 app.use('/api', rtsIndex);
-app.set('view-engine','ejs');
+
 
 app.use(express.static('views'));
 app.use(express.static('css'));
